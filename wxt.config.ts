4c73d3b18461533a62ace850f7436e5c68f2590a
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-10-11 08:06:49 +0530

    Add: manifest properties

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  manifest: {
+    name: 'LinkedIn AI Reply Ext',
+    description: 'Enhances LinkedIn messaging experience powered by AI',
+    version: '1.0',
+    permissions: ['activeTab'],
+  },
 });
