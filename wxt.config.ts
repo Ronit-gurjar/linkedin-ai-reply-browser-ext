5dd8aafa2262b642eeb9b3fe16d98660176c5227
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-10-11 22:09:43 +0530

    Add: permissions and settings for manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,6 +7,20 @@ export default defineConfig({
     name: 'LinkedIn AI Reply Ext',
     description: 'Enhances LinkedIn messaging experience powered by AI',
     version: '1.0',
-    permissions: ['activeTab'],
+    permissions: ['activeTab', 'storage', 'tabs','background'],
+    action: {
+    "default_popup": "popup/index.html"
+    },
+    background: {
+      service_worker: 'background.ts'
+    },
+    content_scripts:[{
+      matches: ['*://www.linkedin.com/*'],
+      "js": ["content-scripts/content.js"],
+      "run_at" : "document_idle"
+    }],
+    content_security_policy: {
+      "extension_pages": "script-src 'self'; object-src 'self';"
+    },
   },
 });
