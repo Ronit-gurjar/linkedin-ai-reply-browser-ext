2dd8dc1ab43a8bc6219d25da6078fd9b654d3167
Ronit-gurjar <ronitgurjar999@gmail.com>
2024-10-13 04:17:51 +0530

    Chore: improve policy and ext details

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,23 +4,25 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    name: 'LinkedIn AI Reply Ext',
+    name: 'LinkedIn AI Reply',
     description: 'Enhances LinkedIn messaging experience powered by AI',
-    version: '1.0',
-    permissions: ['activeTab', 'storage', 'tabs','background'],
+    version: '0.1.0',
+    permissions: ['activeTab', 'storage', 'tabs'],
+    host_permissions: ['*://www.linkedin.com/*'],
     action: {
-    "default_popup": "popup/index.html"
+      default_popup: 'popup/index.html'
     },
     background: {
       service_worker: 'background.ts'
     },
-    content_scripts:[{
+    content_scripts: [{
       matches: ['*://www.linkedin.com/*'],
       "js": ["content-scripts/content.js"],
-      "run_at" : "document_idle"
+      "run_at" : 'document_idle'
     }],
     content_security_policy: {
-      "extension_pages": "script-src 'self'; object-src 'self';"
+      extension_pages: "script-src 'self'; object-src 'self'; trusted-types default allow-duplicates;",
+      sandbox: "sandbox allow-scripts allow-forms allow-popups allow-modals; script-src 'self' 'unsafe-inline' 'unsafe-eval'; child-src 'self'; trusted-types default allow-duplicates;"
     },
   },
 });
